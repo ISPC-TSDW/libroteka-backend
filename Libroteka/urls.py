8235d92741381e17c245d1780af7a65d4f3a22e1
rominasolhaag <rominasolhaag@gmail.com>
2025-05-05 02:38:12 -0300

    Add images to books and edit permissions

--- Libroteka/urls.py
+++ Libroteka/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+
 
 
 
@@ -23,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('books.urls')),
     path('api/', include('books.urls'))
-]
+] 
